29e3fe222e603dbcbac356fdf80fa72f6dea074a
TheRealSeber <rydzseba@gmail.com>
2023-11-27 19:28:09 +0100

    dasdas

---
+++ examples/example_1/main.py
@@ -0,0 +1,34 @@
+from math import pi, ceil, floor
+
+
+class Broski:
+    def __init__(self) -> None:
+        self.hi = "hi_mom"
+
+    def say_hi(self):
+        print(self.hi)
+
+
+if __name__ == "__main__":
+    print("Hello World!")
+    print("Pi is approximately: " + str(pi))
+    very_long_message = """
+    BRO I AM VERY LONG AND I TAKE A LOT OF SPACE IN THIS FILE,
+    YOU CAN'T DO ANYTHING ABOUT THAT HAHAHA"""
+    print("I AM CEILED!!::" + str(ceil(pi)))
+    print("I AM FLOORED!!::" + str(floor(pi)))
+
+    print("I AM FAR AWAY MY FRIEND!!")
+
+    who_am_i = "I AM A VARIABLE"
+
+    print("EVEN WORSE")
+    print("BUT IMA OK")
+
+    def my_function():
+        print("I AM A FUNCTION")
+
+    my_function()
+
+    broski = Broski()
+    broski.say_hi()
